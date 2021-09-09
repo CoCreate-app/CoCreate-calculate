db484c0680f0ba07a3248d285d5ed3b7f8c8eef2
frank pagan <frank@cocreate.app>
2021-09-09 02:15:32 +0000

    fix: CoCreate.config directory, path, name and  public

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,20 +1,25 @@
 module.exports = {
-	"config": {
-		"apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
-		"securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
-		"organization_Id": "5de0387b12e200ea63204d6c",
-		"host": "server.cocreate.app:8088"
-	},
-
+    "config": {
+        "apiKey": "2061acef-0451-4545-f754-60cf8160",
+        "organization_Id": "5ff747727005da1c272740ab",
+        "host": "general.cocreate.app"
+    },
+    
     "sources": [{
             "entry": "./docs/index.html",
             "collection": "files",
             "document_id": "6019bade4eea0817df303b0a",
             "key": "src",
             "data":{
-                "name": "calculation Doc",
-                "domains": ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
-                "path": "/docs/calculation"
+                "name": "index.html",
+                "path": "/docs/calculation/index.html",
+                "domains": [
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/calculation",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "61381ed8829b690010a4f2b2"
             }
         }
     ],
