0954f8dcbcf1357135a8ac09897689b48ede1bd3
Frank pagan <frank@cocreate.app>
2021-11-09 20:17:20 +0000

    feat: supports all valid selectors in attribute calculte

--- src/index.js
+++ src/index.js
@@ -16,156 +16,33 @@ var CoCreateCalculation = {
 
     initElement: function(ele) {
         const self = this;
-        let data_calculation = ele.getAttribute('calculate');
-        let ids = this.getIds(data_calculation);
+        let calculation = ele.getAttribute('calculate');
+        let selectors = this.getSelectors(calculation);
 
-        let selectors = [];
-        for(let i = 0; i < ids.length; i++) {
-            let id = ids[i];
+        for(let i = 0; i < selectors.length; i++) {
+            let id = selectors[i];
 
-            let input = null;
-            try {
-                input = document.querySelector(id);
-            }
-            catch(error) { input = null; }
-
-            if(input) {
+            let inputs = document.querySelectorAll(id);
+            for (let input of inputs){
                 input.addEventListener('input', function() {
                     self.setCalcationResult(ele);
                 });
 
-                if(input.hasAttribute('calculate')) {
-                    input.addEventListener('changedCalcValue', function(e) {
-                        self.setCalcationResult(ele);
-                    });
-                }
-
-            }
-            else {
-                //. add event of special operator
-
-                let selector = this.__getOperatorSelector(id);
-                if(selector) {
-                    selectors.push(selector);
-                }
+                // if(input.hasAttribute('calculate')) {
+                //     input.addEventListener('changedCalcValue', function(e) {
+                //         self.setCalcationResult(ele);
+                //     });
+                // }
             }
             self.setCalcationResult(ele);
         }
-
-        if(selectors.length === 0) {
-            self.setCalcationResult(ele);
-        }
-        if(selectors.length > 0) {
-            document.addEventListener('changedCalcValue', function(e) {
-
-                let isMatched = false;
-                selectors.forEach(selector => {
-                    if(e.target.matches(selector)) {
-                        isMatched = true;
-                        return;
-                    }
-                });
-
-                if(isMatched) {
-                    self.setCalcationResult(ele);
-                }
-            });
-        }
     },
-
-    __getOperatorSelector(value) {
-        let result = /SUM\(\s*([\w\W]+)\s*\)/g.exec(value);
-        if(result && result[1]) {
-            return result[1].trim();
-        }
-        return null;
-    },
-
-    calculationSpecialOperator(value) {
-        let self = this;
-        let sum = null;
-        let result = /SUM\(\s*([\w\W]+)\s*\)/g.exec(value);
-        if(result) {
-            let selector = result[1].trim();
-
-            if(value.trim().indexOf('SUM') == 0) {
-                let elements = document.querySelectorAll(selector);
-                sum = 0;
-                elements.forEach(el => {
-                    let tmpValue = el.getValue(el);
-                    tmpValue = Number(tmpValue);
-                    if(!Number.isNaN(tmpValue)) {
-                        sum += tmpValue;
-                    }
-                });
-            }
-        }
-        return sum;
-    },
-
-
-    setCalcationResult: function(ele) {
-        const { isRealtime } = crud.getAttr(ele);
-        let data_calculation = ele.getAttribute('calculate');
-
-        let calString = this.replaceIdWithValue(data_calculation);
-
-        if(calString) {
-            let result = calculation(calString);
-            
-            if (ele.setValue) {
-                ele.setValue(ele, result)
-				if (isRealtime != "false") {
-                    elements.save(ele);
-                } 
-            }
-            
-            //. set custom event
-            var event = new CustomEvent('changedCalcValue', {
-                bubbles: true,
-            });
-            ele.dispatchEvent(event);
-        }
-
-    },
-
-    replaceIdWithValue: function(data_calculation) {
-        let ids = this.getIds(data_calculation);
-
-        for(let i = 0; i < ids.length; i++) {
-            let id = ids[i];
-
-            let input = null;
-
-            try {
-                input = document.querySelector(id);
-            }
-            catch(error) { input = null; }
-
-            let value = null;
-            if(input) {
-                if(input.getValue)
-                    value = Number(input.getValue(input));
-                else
-                    value = elements.getValue(input);
-            }
-            else {
-                value = this.calculationSpecialOperator(id);
-            }
-
-            if(value != null && !Number.isNaN(value)) {
-                data_calculation = data_calculation.replaceAll('{' + id + '}', value);
-            }
-        }
-
-        return data_calculation;
-    },
-
-    getIds: function(string) {
+    
+    getSelectors: function(string) {
         let tmp = string;
 
-        let ids = [];
-        if(!tmp) return ids;
+        let selectors = [];
+        if(!tmp) return selectors;
         while(tmp.length > 0) {
             let firstIndex = tmp.indexOf('{');
             let secondIndex = tmp.indexOf('}', firstIndex);
@@ -173,21 +50,89 @@ var CoCreateCalculation = {
             if(firstIndex > -1 && secondIndex > -1) {
                 let id = tmp.substring(firstIndex + 1, secondIndex);
 
-                if(ids.indexOf(id) == -1) ids.push(id);
+                if(selectors.indexOf(id) == -1) selectors.push(id);
 
                 tmp = tmp.substring(secondIndex + 1);
 
             }
             else {
-                return ids;
+                return selectors;
             }
         }
 
-        return ids;
+        return selectors;
     },
+
+    getValues: function(calculation) {
+        let selectors = this.getSelectors(calculation);
+
+        for(let i = 0; i < selectors.length; i++) {
+            let selector = selectors[i];
+
+            let value = null;
+            let inputs = document.querySelectorAll(selector);
+            
+            for(let input of inputs){
+                let val = null;
+                if(input.getValue)
+                    val = Number(input.getValue(input));
+                else
+                    val = elements.getValue(input);            
+                val = Number(val);
+                if(!Number.isNaN(value)) {
+                    value += val;
+                }
+            }
+
+            if(value != null && !Number.isNaN(value)) {
+                calculation = calculation.replaceAll('{' + selector + '}', value);
+            }
+        }
+
+        return calculation;
+    },
+    
+    setCalcationResult: function(element) {
+        const { isRealtime } = crud.getAttr(element);
+        let calculation = element.getAttribute('calculate');
+
+        let calString = this.getValues(calculation);
+
+        if(calString) {
+            let result = calculate(calString);
+            
+            // ToDO: input event below triggers save for all input elements but will not save for regular elements
+            if (element.setValue) {
+                element.setValue(element, result)
+				if (isRealtime != "false") {
+                    elements.save(element);
+                } 
+            }
+            else {
+                // if(element.value){
+                    
+                // }
+                // else {
+                    element.innerHTML = result;
+                // }
+            }
+            
+            let inputEvent = new CustomEvent('input', { bubbles: true });
+            Object.defineProperty(inputEvent, 'target', { writable: false, value: element });
+            element.dispatchEvent(inputEvent);
+            
+            //. set custom event
+            // var event = new CustomEvent('changedCalcValue', {
+            //     bubbles: true,
+            // });
+            // element.dispatchEvent(event);
+        }
+
+    },
+    
 };
 
-function calculation(string) {
+function calculate(string) {
     if(!string.match(/[a-z_]/i))
         return eval(string);
 }
